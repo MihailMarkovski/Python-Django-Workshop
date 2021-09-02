cfa12b10af6dd0db7ed986508b99c4e94e69b663
Mihail Markovski <misho5071@gmail.com>
2021-09-02 22:45:51 +0300

    push

--- petstagram/petstagram/settings.py
+++ petstagram/petstagram/settings.py
@@ -85,7 +85,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': 'postgres',
         'USER': 'postgres',
-        'PASSWORD': 'df893ccc',
+        'PASSWORD': ,
         'HOST': '127.0.0.1',
         'PORT': '5432',
     }
